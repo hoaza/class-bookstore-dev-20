6fb034f60107e43920b86f78b575edac37517294
elmarputz <github@elp.name>
2020-02-27 14:49:27 +0100

    [TASK] template structure

--- views/welcome.php
+++ views/welcome.php
@@ -1,1 +1,9 @@
-welcome view
+<?php require_once('views/partials/header.php'); ?>
+<div class="page-header">
+    <h2>Welcome</h2>
+</div>
+
+<p>SCM4 Bookshop</p>
+
+<?php require_once('views/partials/footer.php'); ?>
+
