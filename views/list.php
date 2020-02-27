0b7d58a986f016e2b1bbceb339b2536378860871
elmarputz <github@elp.name>
2020-02-27 17:08:03 +0100

    category list view

--- views/list.php
+++ views/list.php
@@ -3,16 +3,19 @@
 use Data\DataManager;
 $categories = DataManager::getCategories();
 
-print_r($categories);
-die();
 
 require_once('views/partials/header.php'); ?>
 <div class="page-header">
     <h2>List</h2>
 </div>
 
-
-
+<ul class="nav nav-tabs">
+    <?php foreach ($categories as $cat) { ?>
+        <li role="presentation">
+            <a href="#"><?php echo $cat->getName(); ?></a>
+        </li>
+    <?php } ?>    
+</ul>
 
 
 <?php require_once('views/partials/footer.php'); ?>
