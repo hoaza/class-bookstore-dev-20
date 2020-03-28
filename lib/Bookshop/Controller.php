bd46c699b62bf8cb6a193ace992acef0642771b3
Elmar Putz <ep@studiomitte.com>
2020-03-28 15:22:24 +0100

    Forward Request

--- lib/Bookshop/Controller.php
+++ lib/Bookshop/Controller.php
@@ -51,11 +51,37 @@ class Controller extends BaseObject
                 ShoppingCart::remove((int) $_REQUEST['bookId']);
                 Util::redirect();
                 break;
-
-
+            case self::ACTION_LOGIN :
+                if (!AuthenticationManager::authenticate($_REQUEST[self::USER_NAME], $_REQUEST[self::USER_PASSWORD])) {
+                    $this->forwardRequest(array('Invalid user name or password.'));
+                }
+                Util::redirect();
+                break;
 
         }
 
+    }
+
+
+    /**
+     *
+     * @param array $errors : optional assign it to
+     * @param string $target : url for redirect of the request
+     */
+    protected function forwardRequest(array $errors = null, $target = null) {
+        //check for given target and try to fall back to previous page if needed
+        if ($target == null) {
+            if (!isset($_REQUEST[self::PAGE])) {
+                throw new Exception('Missing target for forward.');
+            }
+            $target = $_REQUEST[self::PAGE];
+        }
+        //forward request to target
+        // optional - add errors to redirect and process them in view
+        if (count($errors) > 0)
+            $target .= '&errors=' . urlencode(serialize($errors));
+        header('location: ' . $target);
+        exit();
     }
 
 
