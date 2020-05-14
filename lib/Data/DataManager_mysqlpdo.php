a5c729a686e67cc3d7a8fe5bcc1c1c44b689eebd
Elmar Putz <ep@studiomitte.com>
2020-05-14 15:49:41 +0200

    user methods

--- lib/Data/DataManager_mysqlpdo.php
+++ lib/Data/DataManager_mysqlpdo.php
@@ -1,11 +1,10 @@
 <?php
 
-
 namespace Data;
+
 use Bookshop\Category;
 use Bookshop\Book;
 use Bookshop\User;
-
 
 
 class DataManager implements IDataManager
@@ -13,7 +12,8 @@ class DataManager implements IDataManager
 
     private static $__connection;
 
-    private static function getConnection() {
+    private static function getConnection()
+    {
 
         if (!isset(self::$__connection)) {
 
@@ -30,12 +30,14 @@ class DataManager implements IDataManager
 
     }
 
-    public static function exposeConnection() {
+    public static function exposeConnection()
+    {
         return self::getConnection();
     }
 
 
-    private static function query ($connection, $query, $parameters = []) {
+    private static function query($connection, $query, $parameters = [])
+    {
         $connection->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
         try {
             // SELECT * FROM book WHERE id = ? AND price > ?
@@ -55,23 +57,25 @@ class DataManager implements IDataManager
                 $i++;
             }
             $statement->execute();
-        }
-        catch (\Exception $e) {
+        } catch (\Exception $e) {
             die ($e->getMessage());
         }
         return $statement;
     }
 
 
-    private static function fetchObject ($cursor) {
+    private static function fetchObject($cursor)
+    {
         return $cursor->fetch(\PDO::FETCH_OBJ);
     }
 
-    private static function close($cursor) {
+    private static function close($cursor)
+    {
         $cursor->closeCursor();
     }
 
-    private static function closeConnection() {
+    private static function closeConnection()
+    {
         self::$__connection = null;
     }
 
@@ -82,7 +86,8 @@ class DataManager implements IDataManager
      *
      * @return array of Category-items
      */
-    public static function getCategories() : array {
+    public static function getCategories(): array
+    {
         $categories = [];
         $con = self::getConnection();
         $res = self::query($con, "
@@ -102,10 +107,11 @@ class DataManager implements IDataManager
     /**
      * get the books per category
      *
-     * @param integer $categoryId  numeric id of the category
+     * @param integer $categoryId numeric id of the category
      * @return array of Book-items
      */
-    public static function getBooksByCategory(int $categoryId) : array {
+    public static function getBooksByCategory(int $categoryId): array
+    {
         $books = [];
         $con = self::getConnection();
         $res = self::query($con, "
@@ -135,10 +141,11 @@ class DataManager implements IDataManager
      * on Booleans for more information. Use the === operator for testing
      * the return value of this function." -- http://php.net/manual/en/function.stripos.php
      *
-     * @param string $term  search term: book title string match
+     * @param string $term search term: book title string match
      * @return array of Book-items
      */
-    public static function getBooksForSearchCriteria(string $term) : array {
+    public static function getBooksForSearchCriteria(string $term): array
+    {
 
 
     }
@@ -146,23 +153,37 @@ class DataManager implements IDataManager
     /**
      * get the books per search term – paginated set only
      *
-     * @param string $term  search term: book title string match
-     * @param integer $offset  start at the nth item
-     * @param integer $numPerPage  number of items per page
+     * @param string $term search term: book title string match
+     * @param integer $offset start at the nth item
+     * @param integer $numPerPage number of items per page
      * @return PagingResult
      */
-    public static function getBooksForSearchCriteriaWithPaging(string $term, int $offset, int $numPerPage) : PagingResult {
+    public static function getBooksForSearchCriteriaWithPaging(string $term, int $offset, int $numPerPage): PagingResult
+    {
 
     }
 
     /**
      * get the User item by id
      *
-     * @param integer $userId  uid of that user
+     * @param integer $userId uid of that user
      * @return User | null
      */
-    public static function getUserById(int $userId) { // no return type, cos "null" is not a valid User
-
+    public static function getUserById(int $userId)
+    { // no return type, cos "null" is not a valid User
+        $user = null;
+        $con = self::getConnection();
+        $res = self::query($con, " 
+            SELECT id, userName, passwordHash 
+            FROM users 
+            WHERE id = ?;
+        ", [$userId]);
+        if ($u = self::fetchObject($res)) {
+            $user = new User($u->id, $u->userName, $u->passwordHash);
+        }
+        self::close($res);
+        self::closeConnection($con);
+        return $user;
     }
 
     /**
@@ -170,11 +191,25 @@ class DataManager implements IDataManager
      *
      * note: show for case sensitive and insensitive options
      *
-     * @param string $userName  name of that user - must be exact match
+     * @param string $userName name of that user - must be exact match
      * @return User | null
      */
-    public static function getUserByUserName(string $userName) { // no return type, cos "null" is not a valid User
+    public static function getUserByUserName(string $userName)
+    {
 
+        $user = null;
+        $con = self::getConnection();
+        $res = self::query($con, " 
+            SELECT id, userName, passwordHash 
+            FROM users 
+            WHERE userName = ?;
+        ", [$userName]);
+        if ($u = self::fetchObject($res)) {
+            $user = new User($u->id, $u->userName, $u->passwordHash);
+        }
+        self::close($res);
+        self::closeConnection($con);
+        return $user;
     }
 
     /**
@@ -183,14 +218,16 @@ class DataManager implements IDataManager
      * note: nothing to do here without the database
      * random order id can be used to demonstrate that POST is only executed once
      *
-     * @param integer $userId   id of the ordering user
-     * @param array $bookIds    integers of book ids
-     * @param string $nameOnCard  cc name
-     * @param string $cardNumber  cc number
+     * @param integer $userId id of the ordering user
+     * @param array $bookIds integers of book ids
+     * @param string $nameOnCard cc name
+     * @param string $cardNumber cc number
      * @return integer
      */
-    public static function createOrder(int $userId, array $bookIds, string $nameOnCard, string $cardNumber) : int {
+    public static function createOrder(int $userId, array $bookIds, string $nameOnCard, string $cardNumber): int
+    {
         return rand();
     }
+
 
 }
